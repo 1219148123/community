d73e888563629a80faf3ffd199d6052b38de7fea
hzs <487387645@qq.com>
2019-10-10 16:16:03 +0800

    实现了持久化登录,但是关闭浏览器还是有问题

--- src/main/java/com/hzs/bootlaunch/controller/IndexController.java
+++ src/main/java/com/hzs/bootlaunch/controller/IndexController.java
@@ -1,15 +1,36 @@
 package com.hzs.bootlaunch.controller;
 
 
+import com.hzs.bootlaunch.mapper.UserMapper;
+import com.hzs.bootlaunch.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class IndexController {
-
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String greeting() {
+    public String greeting(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 
